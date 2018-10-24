76bed0fcf7fd27d2de12f01ab23573a0223c25aa
Brielle <jasperrecitas05@gmail.com>
2018-10-25 02:44:46 +0800

    Add touch support for delete,quantity,multiselect buttons <Removed Mouse Click for those buttons>

--- CompleetKassa.Views/Modules/Sales/SalesView.xaml.cs
+++ CompleetKassa.Views/Modules/Sales/SalesView.xaml.cs
@@ -4,6 +4,8 @@ using CompleetKassa.ViewModels;
 using System.Diagnostics;
 using System;
 using System.Windows.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CompleetKassa.Views
 {
@@ -52,7 +54,7 @@ namespace CompleetKassa.Views
 
             if (time>=1)
             {
-
+              
                 dt.Stop();
 
                 if(button_name.Equals("DeleteButton"))
@@ -65,10 +67,12 @@ namespace CompleetKassa.Views
 
         }
 
-        string button_name;
+        string button_name= "";
    
         private void Button_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
+        
+
             var button = (Button)sender;
             button_name = button.Name.ToString();
 
@@ -77,37 +81,11 @@ namespace CompleetKassa.Views
             dt.Start();
 
 
-         
+
 
         }
 
         private void Button_PreviewMouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            dt.Stop();
-            if (time<1)
-            {
-                if (button_name.Equals("DeleteButton"))
-                    ButtonDelete.Command.Execute(ButtonDelete.CommandParameter);
-                else if (button_name.Equals("ButtonQuantitySelection"))
-                    ButtonQuantityClick.Command.Execute(ButtonQuantityClick.CommandParameter);
-                else
-                    ButtonSelectMultiple.Command.Execute(ButtonSelectMultiple.CommandParameter);
-
-        
-            }
-
-
-
-
-        }
-
-        private void Button_TouchDown(object sender, System.Windows.Input.TouchEventArgs e)
-        {
-            time = 0;
-            dt.Start();
-        }
-
-        private void Button_TouchUp(object sender, System.Windows.Input.TouchEventArgs e)
         {
             dt.Stop();
             if (time < 1)
@@ -118,6 +96,44 @@ namespace CompleetKassa.Views
                     ButtonQuantityClick.Command.Execute(ButtonQuantityClick.CommandParameter);
                 else
                     ButtonSelectMultiple.Command.Execute(ButtonSelectMultiple.CommandParameter);
+
+
+            }
+
+
+
+
+        }
+
+
+        //For LongTouch Purposes
+        private void DeleteButton_StylusDown(object sender, StylusDownEventArgs e)
+        {
+            
+
+            var button = (Button)sender;
+            button_name = button.Name.ToString();
+
+            time = 0;
+            dt.Start();
+
+
+        }
+
+        private void DeleteButton_StylusUp(object sender, StylusEventArgs e)
+        {
+            dt.Stop();
+            if (time < 1)
+            {
+
+                if (button_name.Equals("DeleteButton"))
+                    ButtonDelete.Command.Execute(ButtonDelete.CommandParameter);
+                else if (button_name.Equals("ButtonQuantitySelection"))
+                    ButtonQuantityClick.Command.Execute(ButtonQuantityClick.CommandParameter);
+                else
+                    ButtonSelectMultiple.Command.Execute(ButtonSelectMultiple.CommandParameter);
+
+
             }
         }
     }
